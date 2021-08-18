2ed9df4fc15561e5a9dc35e81cd31c8579bbcfa9
JorgeCarreon <jorgejaziel1234@gmail.com>
2021-08-17 22:48:21 -0500

    Posibles Cambios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
-/* ----- MOBILE AND TABLET SIDEBAR ----- */
-const burger_btn = document.getElementById('burger_btn');
-const sidebar = document.getElementById('sidebar');
-const overlay = document.getElementById('overlay');
+window.onscroll = function(){
 
-burger_btn.addEventListener('click', function() {
-  sidebar.classList.toggle('sidebar_open');
-  overlay.classList.toggle('overlay_open');
-})
+  scroll = document.documentElement.scrollTop;
 
-overlay.addEventListener('click', function() {
-  sidebar.classList.toggle('sidebar_open');
-  overlay.classList.toggle('overlay_open');
-})
+  header = document.getElementById("header");
+
+  if (scroll > 20){
+      header.classList.add('nav_mod');
+  }else if (scroll < 20){
+      header.classList.remove('nav_mod');
+  }
+
+}
+
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+  menu = document.getElementById("header");
+  body = document.getElementById("container__all");
+  nav = document.getElementById("nav");
+
+function mostrar_menu(){
+
+  body.classList.toggle('move_content');
+  menu.classList.toggle('move_content');
+  nav.classList.toggle('move_nav');
+}
+
+window.addEventListener("resize", function(){
+
+  if (window.innerWidth > 760)  {
+      body.classList.remove('move_content');
+      menu.classList.remove('move_content');
+      nav.classList.remove('move_nav');
+  }
+
+});
